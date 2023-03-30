03bf73ec31518dbf0d81ab1f3600fdb89dc7719f
OscarDeLeon19 <oscar99deleon19@gmail.com>
2023-03-30 10:43:46 -0600

    Agregamos informacion a la pagina de maizGuatemala, ademas de un grafico

--- Hola-Mundo-Web/src/app/app.module.ts
+++ Hola-Mundo-Web/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { MaizGuatemalaComponent } from './pages/maiz-guatemala/maiz-guatemala.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MaizGuatemalaComponent } from './pages/maiz-guatemala/maiz-guatemala.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
